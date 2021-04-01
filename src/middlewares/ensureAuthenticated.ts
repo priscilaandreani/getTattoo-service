3a4c743daff459f0f7320cf15d6877c163b7ae94
Priscila Ravanelli <priscila.andreani@b2wdigital.com>
2021-04-01 17:02:49 -0300

    (ensureAuthenticated) add user token id

--- src/middlewares/ensureAuthenticated.ts
+++ src/middlewares/ensureAuthenticated.ts
@@ -2,6 +2,12 @@ import { NextFunction, Request, Response } from 'express';
 import { verify } from 'jsonwebtoken';
 import authConfig from '../config/auth';
 
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
 export default function ensureAuthenticated(
   request: Request,
   response: Response,
@@ -22,7 +28,12 @@ export default function ensureAuthenticated(
   try {
     const decoded = verify(token, authConfig.jwt.secret);
 
-    console.log(decoded);
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    }
+
     return next();
   } catch (err) {
     throw new Error('Invalid JWT token');
