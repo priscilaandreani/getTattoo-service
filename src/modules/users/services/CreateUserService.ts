650a654ba9ffe167bd887a0f241f65b4e3ccc29c
priscila andreani <priscilaandreani@gmail.com>
2021-11-03 14:17:30 -0300

    refact(Hash): isolate password hash

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -4,6 +4,7 @@ import AppError from '@shared/errors/AppError';
 import User from '@modules/users/infra/typeorm/entities/User';
 import { inject, injectable } from 'tsyringe';
 import IUserRepository from '../repositories/IUserRepository';
+import { IHashProvider } from '../providers/HashProvider/models/IHashProvider';
 
 interface Request {
   name: string;
@@ -15,7 +16,10 @@ interface Request {
 export default class CreateUserService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUserRepository
+    private usersRepository: IUserRepository,
+
+    @inject('HashProvider')
+    private iHashProvider: IHashProvider
   ) {}
 
   public async execute({ name, email, password }: Request): Promise<User> {
@@ -25,7 +29,7 @@ export default class CreateUserService {
       throw new AppError('Email address already used.');
     }
 
-    const hashedPassword = await hash(password, 8);
+    const hashedPassword = await this.iHashProvider.generateHash(password);
 
     const user = await this.usersRepository.create({
       name,
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,12 @@
+import { container } from 'tsyringe';
+
+import '@modules/users/providers';
+
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import IAppointmentsRepository from '@modules/appointments/repositories/iAppointmentsRepository';
+
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRespository';
 import IUserRepository from '@modules/users/repositories/IUserRepository';
-import { container } from 'tsyringe';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
